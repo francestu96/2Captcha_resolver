fc48c51eba7b75e49b2eb9308f9a2798adfa4419
Francesco Stucci <francesco.stucci@horsa.it>
2021-01-04 18:11:56 +0100

    refactor

--- ocr.py
+++ ocr.py
@@ -1,5 +1,3 @@
-from ctcLayer import CTCLayer
-import utils
 import os
 import matplotlib.pyplot as plt
 
@@ -11,6 +9,102 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+class CTCLayer(layers.Layer):
+    def __init__(self, name=None):
+        super().__init__(name=name)
+        self.loss_fn = keras.backend.ctc_batch_cost
+        
+    def call(self, y_true, y_pred):
+        # Compute the training-time loss value and add it
+        # to the layer using `self.add_loss()`.
+        batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+        input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+        label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+        
+        input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+        label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+        
+        loss = self.loss_fn(y_true, y_pred, input_length, label_length)
+        self.add_loss(loss)
+        
+        # At test, just return the computed predictions
+        return y_pred
+        
+def build_model():
+    # Inputs the model
+    input_img = layers.Input(
+        shape=(img_width, img_height, 1), name="image", dtype="float32"
+    )
+    labels = layers.Input(name="label", shape=(None,), dtype="float32")
+    
+    # First conv block
+    x = layers.Conv2D(32, (3, 3), activation="relu", kernel_initializer="he_normal", padding="same", name="Conv1")(input_img)
+    x = layers.MaxPooling2D((2, 2), name="pool1")(x)
+    
+    # Second conv block
+    x = layers.Conv2D(64, (3, 3), activation="relu", kernel_initializer="he_normal", padding="same", name="Conv2")(x)
+    x = layers.MaxPooling2D((2, 2), name="pool2")(x)
+    
+    # We have used two max pool with pool size and strides 2.
+    # Hence, downsampled feature maps are 4x smaller.
+    # The number of filters in the last layer is 64. 
+    # Reshape accordingly before passing the output to the RNN part of the model
+    new_shape = ((img_width // 4), (img_height // 4) * 64)
+    x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
+    x = layers.Dense(64, activation="relu", name="dense1")(x)
+    x = layers.Dropout(0.2)(x)
+    
+    # RNNs
+    x = layers.Bidirectional(layers.LSTM(128, return_sequences=True, dropout=0.25))(x)
+    x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, dropout=0.25))(x)
+    
+    # Output layer
+    x = layers.Dense(len(characters) + 1, activation="softmax", name="dense2")(x)
+    
+    # Add CTC layer for calculating CTC loss at each step
+    output = CTCLayer(name="ctc_loss")(labels, x)
+    
+    # Define the model 
+    model = keras.models.Model(
+        inputs=[input_img, labels], outputs=output, name="ocr_model_v1"
+    )
+    # Optimizer
+    opt = keras.optimizers.Adam()
+    # Compile the model and return
+    model.compile(optimizer=opt)
+    return model
+
+def split_data(images, labels, train_size=0.9, shuffle=True):
+    # 1. Get the total size of the dataset
+    size = len(images)
+    # 2. Make an indices array and shuffle it, if required
+    indices = np.arange(size)
+    if shuffle:
+        np.random.shuffle(indices)
+    # 3. Get the size of training samples
+    train_samples = int(size * train_size)
+    # 4. Split data into training and validation sets
+    x_train, y_train = images[indices[:train_samples]], labels[indices[:train_samples]]
+    x_valid, y_valid = images[indices[train_samples:]], labels[indices[train_samples:]] 
+    return x_train, x_valid, y_train, y_valid
+
+def encode_single_sample(img_path, label):
+    # 1. Read image
+    img = tf.io.read_file(img_path)
+    # 2. Decode and convert to grayscale
+    img = tf.io.decode_png(img, channels=1)
+    # 3. Convert to float32 in [0, 1] range
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # 4. Resize to the desired size
+    img = tf.image.resize(img, [img_height, img_width])
+    # 5. Transpose the image because we want the time
+    # dimension to correspond to the width of the image.
+    img = tf.transpose(img, perm=[1, 0, 2])
+    # 6. Map the characters in label to numbers
+    label = char_to_num(tf.strings.unicode_split(label, input_encoding="UTF-8"))
+    # 7. Return a dict as our model is expecting two inputs
+    return {"image": img, "label": label}
+    
 # Path to the data directory
 data_dir = Path("./captcha_images_v2/")
 
@@ -65,7 +159,7 @@ num_to_char = layers.experimental.preprocessing.StringLookup(
 
 
 # Splitting data into training and validation sets
-X_train, X_valid, y_train, y_valid = utils.split_data(np.array(images), np.array(labels))
+X_train, X_valid, y_train, y_valid = split_data(np.array(images), np.array(labels))
 
 print(f"X_Train shape: {X_train.shape}")
 print(f"y_Train shape: {y_train.shape}")
@@ -75,7 +169,7 @@ print(f"y_valid shape: {y_valid.shape}")
 train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
 train_dataset = (
     train_dataset.map(
-        utils.encode_single_sample, num_parallel_calls=tf.data.experimental.AUTOTUNE
+        encode_single_sample, num_parallel_calls=tf.data.experimental.AUTOTUNE
     )
     .batch(batch_size)
     .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
@@ -84,7 +178,7 @@ train_dataset = (
 validation_dataset = tf.data.Dataset.from_tensor_slices((X_valid, y_valid))
 validation_dataset = (
     validation_dataset.map(
-        utils.encode_single_sample, num_parallel_calls=tf.data.experimental.AUTOTUNE
+        encode_single_sample, num_parallel_calls=tf.data.experimental.AUTOTUNE
     )
     .batch(batch_size)
     .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
@@ -101,50 +195,6 @@ for batch in train_dataset.take(1):
         ax[i // 4, i % 4].set_title(label)
         ax[i // 4, i % 4].axis("off")
 plt.show()
-    
-def build_model():
-    # Inputs the model
-    input_img = layers.Input(
-        shape=(img_width, img_height, 1), name="image", dtype="float32"
-    )
-    labels = layers.Input(name="label", shape=(None,), dtype="float32")
-    
-    # First conv block
-    x = layers.Conv2D(32, (3, 3), activation="relu", kernel_initializer="he_normal", padding="same", name="Conv1")(input_img)
-    x = layers.MaxPooling2D((2, 2), name="pool1")(x)
-    
-    # Second conv block
-    x = layers.Conv2D(64, (3, 3), activation="relu", kernel_initializer="he_normal", padding="same", name="Conv2")(x)
-    x = layers.MaxPooling2D((2, 2), name="pool2")(x)
-    
-    # We have used two max pool with pool size and strides 2.
-    # Hence, downsampled feature maps are 4x smaller.
-    # The number of filters in the last layer is 64. 
-    # Reshape accordingly before passing the output to the RNN part of the model
-    new_shape = ((img_width // 4), (img_height // 4) * 64)
-    x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
-    x = layers.Dense(64, activation="relu", name="dense1")(x)
-    x = layers.Dropout(0.2)(x)
-    
-    # RNNs
-    x = layers.Bidirectional(layers.LSTM(128, return_sequences=True, dropout=0.25))(x)
-    x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, dropout=0.25))(x)
-    
-    # Output layer
-    x = layers.Dense(len(characters) + 1, activation="softmax", name="dense2")(x)
-    
-    # Add CTC layer for calculating CTC loss at each step
-    output = CTCLayer(name="ctc_loss")(labels, x)
-    
-    # Define the model 
-    model = keras.models.Model(
-        inputs=[input_img, labels], outputs=output, name="ocr_model_v1"
-    )
-    # Optimizer
-    opt = keras.optimizers.Adam()
-    # Compile the model and return
-    model.compile(optimizer=opt)
-    return model
 
 # Get the model
 model = build_model()
