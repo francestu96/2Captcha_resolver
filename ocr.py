9294506f962fde905756a94429779e80b8ac9a95
Francesco Stucci <francesco.stucci@horsa.it>
2021-01-06 16:33:48 +0100

    save model

--- ocr.py
+++ ocr.py
@@ -9,12 +9,14 @@ from collections import Counter
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-    
+
 # Path to the data directory
 data_dir = Path("./captcha_images/")
+# Path to the model directory
+model_dir = Path("./model/")
 
 # Get list of all the images
-images = sorted(list(map(str, list(data_dir.glob("*.png")))))
+images = list(map(str, list(data_dir.glob("*.png"))))
 labels = [img.split(os.path.sep)[-1].split(".png")[0] for img in images]
 characters = [x for x in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789']
 
@@ -24,12 +26,6 @@ batch_size = 24
 # Desired image dimensions
 img_width = 250
 img_height = 80
-
-# Factor by which the image is going to be downsampled by the convolutional blocks.
-# We will be using two convolution blocks and each block will have a pooling layer
-# which downsample the features by a factor of 2.
-# Hence total downsampling factor would be 4.
-downsample_factor = 4
 
 # Maximum length of any captcha in the dataset
 max_length = max([len(label) for label in labels])
@@ -47,11 +43,6 @@ num_to_char = layers.experimental.preprocessing.StringLookup(
 
 # Splitting data into training and validation sets
 X_train, X_valid, y_train, y_valid = split_data(np.array(images), np.array(labels))
-
-print(f"X_Train shape: {X_train.shape}")
-print(f"y_Train shape: {y_train.shape}")
-print(f"X_valid shape: {X_valid.shape}")
-print(f"y_valid shape: {y_valid.shape}")
 
 train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
 train_dataset = (
@@ -71,64 +62,55 @@ validation_dataset = (
     .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 )
 
-_, ax = plt.subplots(4, 4, figsize=(10, 5))
-for batch in train_dataset.take(1):
-    images = batch["image"]
-    labels = batch["label"]
-    for i in range(16):
-        img = (images[i] * 255).numpy().astype("uint8")
-        label = tf.strings.reduce_join(num_to_char(labels[i])).numpy().decode("utf-8")
-        ax[i // 4, i % 4].imshow(img[:, :, 0].T, cmap="gray")
-        ax[i // 4, i % 4].set_title(label)
-        ax[i // 4, i % 4].axis("off")
-plt.show()
+try:
+    prediction_model = keras.models.load_model(model_dir, compile=False)
+except OSError:
+    # Get the model
+    model = build_model(img_width, img_height, characters)
 
-# Get the model
-model = build_model(img_width, img_height, characters)
-model.summary()
+    epochs = 200
+    early_stopping_patience = 10
 
-epochs = 200
-early_stopping_patience = 10
+    # Add early stopping
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience=early_stopping_patience, restore_best_weights=True
+    )
 
-# Add early stopping
-early_stopping = keras.callbacks.EarlyStopping(
-    monitor="val_loss", patience=early_stopping_patience, restore_best_weights=True
-)
+    # Train the model
+    history = model.fit(
+        train_dataset, 
+        validation_data=validation_dataset,
+        epochs=epochs,
+        callbacks=[early_stopping]
+    )
 
-# Train the model
-history = model.fit(
-    train_dataset, 
-    validation_data=validation_dataset,
-    epochs=epochs,
-    callbacks=[early_stopping]
-)
+    # Get the prediction model by extracting layers till the output layer
+    prediction_model = keras.models.Model(
+        model.get_layer(name="image").input, model.get_layer(name="dense2").output
+    )
 
-# Get the prediction model by extracting layers till the output layer
-prediction_model = keras.models.Model(
-    model.get_layer(name="image").input, model.get_layer(name="dense2").output
-)
-prediction_model.summary()
+    prediction_model.save(model_dir)
 
 # Let's check results on some validation samples
-for batch in validation_dataset.take(1):
-    batch_images = batch["image"]
-    batch_labels = batch["label"]
+# for batch in validation_dataset.take(1):
+#     batch_images = batch["image"]
+#     batch_labels = batch["label"]
     
-    preds = prediction_model.predict(batch_images)
-    pred_texts = decode_batch_predictions(preds, max_length, num_to_char)
+#     preds = prediction_model.predict(batch_images)
+#     pred_texts = decode_batch_predictions(preds, max_length, num_to_char)
     
-    orig_texts = []
-    for label in batch_labels:
-        label = tf.strings.reduce_join(num_to_char(label)).numpy().decode("utf-8")
-        orig_texts.append(label)
+#     orig_texts = []
+#     for label in batch_labels:
+#         label = tf.strings.reduce_join(num_to_char(label)).numpy().decode("utf-8")
+#         orig_texts.append(label)
         
-    _, ax = plt.subplots(4, 4, figsize=(15, 5))
-    for i in range(len(pred_texts)):
-        img = (batch_images[i, :, :, 0] * 255).numpy().astype(np.uint8)
-        img = img.T
-        title = f"Prediction: {pred_texts[i]}"
-        ax[i // 4, i % 4].imshow(img, cmap="gray")
-        ax[i // 4, i % 4].set_title(title)
-        ax[i // 4, i % 4].axis("off")        
+#     _, ax = plt.subplots(4, 4, figsize=(15, 5))
+#     for i in range(len(pred_texts)):
+#         img = (batch_images[i, :, :, 0] * 255).numpy().astype(np.uint8)
+#         img = img.T
+#         title = f"Prediction: {pred_texts[i]}"
+#         ax[i // 4, i % 4].imshow(img, cmap="gray")
+#         ax[i // 4, i % 4].set_title(title)
+#         ax[i // 4, i % 4].axis("off")        
 
-plt.show()
+# plt.show()
